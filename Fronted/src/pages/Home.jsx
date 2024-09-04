3f36cd7ebd039433992bd3a42c0b9e8cc58f8e82
Gourab <gourabbistu2002@gmail.com>
2024-09-04 15:47:06 +0530

    para

--- Fronted/src/pages/Home.jsx
+++ Fronted/src/pages/Home.jsx
@@ -6,6 +6,7 @@ function Home() {
     <div>Home</div>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates vitae nulla, necessitatibus nisi minima repellendus autem incidunt debitis voluptatum similique mollitia rem!</p>
     <h1> Now its edited </h1>
+    <p>This is a paragraph</p>
     </>
   )
 }
