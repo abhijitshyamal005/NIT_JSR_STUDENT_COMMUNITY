79a22c15dcc4af8800b6244047ab253020671f57
Gourab <gourabbistu2002@gmail.com>
2024-09-04 15:38:38 +0530

    routh

--- Fronted/src/pages/Home.jsx
+++ Fronted/src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
+    <>
+
     <div>Home</div>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates vitae nulla, necessitatibus nisi minima repellendus autem incidunt debitis voluptatum similique mollitia rem!</p>
+    </>
   )
 }
 
