cdb820a9d23750c24c5b090dcadb86b4e6d4492e
Gourab <gourabbistu2002@gmail.com>
2024-09-04 15:23:30 +0530

    first commit

---
+++ Fronted/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+
+const router = {
+  home: '/',
+  notFound: '/not-found',
+  // Add more routes as needed
+}
+
+const App = () => {
+  return (
+      <Routes>
+        <Route path='/'element={<Home />} />
+        <Route path='/not-found' element={<NotFound />} />
+        {/* Add more routes here */}
+        <Route path="*" element={<Navigate to={router.notFound} replace />} />
+      </Routes>
+  )
+}
+
+export default App;
+// instruction to install tailwindcss
+// npm install tailwindcss postcss autoprefixer
+// npx tailwindcss init -p
+// npm install tailwindcss postcss autoprefixer
+// tailwind config file
+
