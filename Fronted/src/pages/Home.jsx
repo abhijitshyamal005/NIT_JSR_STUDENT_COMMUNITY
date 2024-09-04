4838a4e8f382178f0d86aee1b7aab5c9c73806a5
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2024-09-04 15:56:33 +0530

    commit

--- Fronted/src/pages/Home.jsx
+++ Fronted/src/pages/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates vitae nulla, necessitatibus nisi minima repellendus autem incidunt debitis voluptatum similique mollitia rem!</p>
     <h1> Now its edited </h1>
     <p>This is a paragraph</p>
+    <h2> Frock check</h2>
     </>
   )
 }
