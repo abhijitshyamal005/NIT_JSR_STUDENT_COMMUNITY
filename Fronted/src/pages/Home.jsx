68b03d756e197cbf80b3a2630651b86d9fb5ea96
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2024-09-06 01:02:32 +0530

    Create About

--- Fronted/src/pages/Home.jsx
+++ Fronted/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Hero1 from "../components/Hero1"
 import Features1 from "../components/Features1"
 import Banner1 from "../components/Banner1"
 import Features2 from "../components/Features2"
+import About from "../components/About"
 
 
 function Home() {
@@ -16,8 +17,7 @@ function Home() {
     <Features1/>
     <Banner1/>
     <Features2/>
-
-    
+    <About/>
     <Footer/>
     </>
   )
