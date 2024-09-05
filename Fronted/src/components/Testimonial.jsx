1e115fc56fb854a4ae76ab4e295be07867bef470
Gourab <gourabbistu2002@gmail.com>
2024-09-05 22:15:08 +0530

    sds

--- Fronted/src/components/Testimonial.jsx
+++ Fronted/src/components/Testimonial.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 function Testimonial() {
-  return (
+  return (  
     <div>
-        
-        
+      
     </div>
+
   )
 }
 
--- Fronted/src/pages/Home.jsx
+++ Fronted/src/pages/Home.jsx
@@ -2,12 +2,14 @@
 import Navbar from "../components/Navbar"
 
 import Footer from "../components/Footer"
+import Testimonial from "../components/Testimonial"
 
 
 function Home() {
   return (
     <>
     <Navbar/>
+    <Testimonial/>
     <Footer/>
     </>
   )
