2f6217c56f23abe55752abbab9d43c2aacb817c8
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2024-09-05 20:42:57 +0530

    Banner1 by abhi

--- Fronted/src/pages/Home.jsx
+++ Fronted/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import Hero1 from "../components/Hero1"
 import Features1 from "../components/Features1"
+import Banner1 from "../components/Banner1"
 
 
 function Home() {
@@ -12,12 +13,9 @@ function Home() {
     <Navbar/>
     <Hero1/>
     <Features1/>
+    <Banner1/>
 
-    <div>Home</div>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates vitae nulla, necessitatibus nisi minima repellendus autem incidunt debitis voluptatum similique mollitia rem!</p>
-    <h1> Now its edited </h1>
-    <p>This is a paragraph</p>
-    <h2> Frock check</h2>
+    
     <Footer/>
     </>
   )
